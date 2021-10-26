4a8f0c04f1b3264f0d9620ebf34dd2a3a4ddac3d
AdelaAlmazan <64912000+AdelaAlmazan@users.noreply.github.com>
2021-10-25 21:25:35 -0600

    Holamundo Unidad2 DWFS

---
+++ holamundo.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("Hola mundo desde Node.Js");
+
+console.log(2 * 5);
+
+//console.log(window);
+
+console.log(global);
+
+setInterval(function () {
+  console.log("hola nodejs");
+}, 1000);
